2dcb2ec84e350477235927b5128b3a51211b6fb0
christophra <christoph@raab.io>
2018-04-16 00:53:06 +0200

    Update python scraper for new site.

--- menuparser.py
+++ menuparser.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python27
+
+###########################
+# Imports
+###########################
+
 import io
 import os
+import string
 import json
 import logging
 import datetime
@@ -9,16 +15,16 @@ import lxml.html
 from lxml.cssselect import CSSSelector
 from multiprocessing.dummy import Pool as ThreadPool
 
+###########################
+# Configuration
+###########################
 
 # Path where the JSONs will get written. Permissions are your job.
-SAVE_PATH = '/var/www/ssl/files/vub-resto/'
-
+#SAVE_PATH = '/var/www/ssl/files/vub-resto/'
+SAVE_PATH = './'
 
 # Urls of the pages that will get parsed
-URL_ETTERBEEK_NL = 'https://my.vub.ac.be/resto/etterbeek'
-URL_ETTERBEEK_EN = 'https://my.vub.ac.be/restaurant/etterbeek'
-URL_JETTE_NL = 'https://my.vub.ac.be/resto/jette'
-URL_JETTE_EN = 'https://my.vub.ac.be/restaurant/jette'
+URL = 'https://student.vub.be/menu-vub-student-restaurant'
 
 
 # Mapping of colors for the menus.
@@ -30,6 +36,8 @@ COLOR_MAPPING = {
     'dag menu': '#68b6f3',  # blue
     'dagmenu': '#68b6f3',  # blue
     'health': '#ff9861',  # orange
+    'vis': '#ff9861',  # orange
+    'fish': '#ff9861',  # orange
     'menu 2': '#cc93d5',  # purple
     'meals of the world': '#cc93d5',  # purple
     'fairtrade': '#cc93d5',  # purple
@@ -41,102 +49,165 @@ COLOR_MAPPING = {
     'wok': '#6c4c42',  # brown
 }
 
+CAMPUS_NAMES = ['etterbeek','jette']
 
-# Months in Dutch, to allow the parsing of the (Dutch) site
-MONTHS = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli',
-          'augustus', 'september', 'oktober', 'november', 'december']
-LOCAL_MONTHS = {month: i for i, month in enumerate(MONTHS, 1)}
-
-
-def is_veggiedag_img(img):
-    return img and 'veggiedag' in img.get('src', '')
-
+###########################
+# Basic parsing functions
+###########################
 
 def normalize_text(text):
+    r"""Remove special characters from text, preparing it for JSON.
+    """
     return text.replace(u'\xa0', u' ').strip()
 
+def check_title(line):
+    r"""Check whether line matches a title for a restaurant's week menu.
+    Return campus.language or None.
+    """
+    # verify format
+    line = line.lower()
+    has_menu  = ('menu' in line)
+    has_campus = any([p in line for p in CAMPUS_NAMES])
+    if not (has_menu and has_campus):
+        return None
+    
+    campus = 'unknown'
+    # determine place
+    for c in CAMPUS_NAMES:
+        if c in line: campus = c
+    
+    # determine language
+    language = 'unknown'
+    if 'week menu' in line:
+        language = 'en'
+    if 'weekmenu' in line:
+        language = 'nl' 
+        
+    return '{0}.{1}'.format(campus,language)
 
-def parse_restaurant(name, url):
-    data = []
+def check_date(line):
+    r"""Check whether date matches D(D).M(M).(YY)YY(:).
+    Return corresponding datetime.date or None.
+    """
+    line = line.rstrip(':') # remove possible trailing colon
+    parts = line.split('.') # split by separator
+    parts = list(filter(lambda s:s.isdigit(), parts)) # remove any that are not numbers
+    triplet = len(parts)==3
+    if not triplet:
+        return None
+    dd, mm, yyyy = tuple(list(map(int, parts)))
+    if (yyyy%1000)<18:
+        return None
+    yyyy = 2000 + (yyyy%1000)
+    return datetime.date(yyyy, mm, dd)
 
-    # Construct CSS Selectors
-    sel_day_divs = CSSSelector('#content .views-row')
-    sel_date_span = CSSSelector('.date-display-single')
-    sel_tablerows = CSSSelector('table tr')
-    sel_img = CSSSelector('img')
+def parse_menu(m):
+    r"""Parse a single menu entry of the format Menu name: Dish name.
+    Return {'name':'Menu name',
+            'dish':'Dish name',
+            'color':color code}
+            or None.
+    """
+    m = m.split(':')
 
+    menu_name = normalize_text(m[0])
+    menu_dish = normalize_text(':'.join(m[1:]))
+
+    menu_name = menu_name.replace(' van de week','') # make uniform names
+    menu_name = menu_name.replace(' of the week','') # between Etterbeek & Jette
+
+
+    menu_color = COLOR_MAPPING.get(menu_name.lower(), None)
+    if menu_color is None:
+        logging.warning("No color found for the menu {0}".format(menu_name))
+        menu_color = DEFAULT_COLOR
+    return {'name': menu_name,
+            'dish': menu_dish,
+            'color': menu_color}
+
+###########################
+# Combining functions
+###########################
+
+def load_and_split(url):
+    r"""First step:
+    Load page from URL, build the DOM tree,
+    and split it by the different week menus it contains.
+    """
+    data = {}
+    
+    # Construct CSS selectors
+    sel_resto = CSSSelector('div.pg-tab')
+    sel_content = CSSSelector('div.rd-content-holder')
+    sel_resto_title = CSSSelector('h2')
+    
     # Request and build the DOM Tree
     r = requests.get(url)
     tree = lxml.html.fromstring(r.text)
+    
+    # Iterate over restaurants
+    for div in sel_resto(tree):
+        # Split into sections
+        sections = sel_content(div)
+        # First is the title
+        resto_title = sel_resto_title(sections[0])[0].text_content()
+        # Parse into location.language e.g. etterbeek.nl
+        resto_key = check_title(resto_title)
+        if resto_key is None:
+            logging.exception('Failed to extract restaurant title from {0}                               at URL {1}'.format(div.text_content(), url))
+            continue
+        # Store daily menus
+        data[resto_key] = sections[1:]
+    return data
 
-    # Apply selector to get divs representing 1 day
-    day_divs = sel_day_divs(tree)
-    for day_div in day_divs:
+def parse_restaurant(name, week):
+    r"""Parse the weekly menu for one restaurant.
+    """
+    data = []
+    
+    sel_date = CSSSelector('p')
+    sel_meals = CSSSelector('ul li')
+    
+    prev_date = datetime.date(2000,1,1)
+    
+    for day in week:
         menus = []
-        # Apply selector to get date span (contains date string of day)
-        date_span = sel_date_span(day_div)
-        # date string should be format '29 september 2014', normally
-        date_string = normalize_text(date_span[0].text_content()).lower()
-        date_components = date_string.split()[1:]
-        month_name = normalize_text(date_components[1]).lower()
-        month = LOCAL_MONTHS.get(date_components[1], None)
-        if month:
-            date = datetime.date(int(date_components[2]),  # year
-                                 month,  # month
-                                 int(date_components[0]))  # day
+
+        date_string = sel_date(day)[0].text_content()
+        date = check_date(date_string)
+        if date is not None:
+            prev_date = date # store for next iteration
         else:
-            # If we couldn't find a month, we try to use the previous date
-            logging.warning("{0} - Failed to get a month \
-                             for the month_name {1} ".format(name, month_name))
+            # If we couldn't parse the date, we try to use the previous date
+            logging.warning("{0} - Failed to parse date {1}".format(name, date_string))
             try:
-                prev_date_components = map(int, data[-1]['date'].split('-'))
-                prev_date = datetime.date(prev_date_components[0],  # year
-                                          prev_date_components[1],  # month
-                                          prev_date_components[2])  # day
                 date = prev_date + datetime.timedelta(days=1)
+                prev_date = date
             except Exception:
                 # If we can't find any date, we'll skip the day
-                logging.exception("{0} - Couldn't derive date \
-                                  from previous dates".format(name))
-                continue
+                logging.exception("{0} - Couldn't derive date                                       from previous dates".format(name))
 
-        # Get the table rows
-        tablerows = sel_tablerows(day_div)
-        try:
-            for tr in tablerows:
-                tds = tr.getchildren()
-                menu_name = normalize_text(tds[0].text_content())
-                menu_dish = normalize_text(tds[1].text_content())
-                # Sometimes there is no menu name,
-                # but just an image (e.g., for "Veggiedag")
-                if not menu_name:
-                    img = sel_img(tds[0])
-                    img = img[0] if img else None
-                    menu_name = 'Veggiedag' if is_veggiedag_img(img) else 'Menu'
-                menu_color = COLOR_MAPPING.get(menu_name.lower(), None)
-                if menu_color is None:
-                    logging.warning(name + " - No color found for the menu: '" +
-                                    menu_name + "' (" + str(date) + ")")
-                    menu_color = DEFAULT_COLOR
-                if menu_dish:
-                    menus.append({'name': menu_name,
-                                  'dish': menu_dish,
-                                  'color': menu_color})
-        except:
-            # cba
-            pass
+        meals = sel_meals(day)
+        for m in meals:
+            m = parse_menu(m.text_content())
+            #if m['dish']:
+            menus.append(m)
+
         data.append({'date': str(date), 'menus': menus})
     return data
 
+###########################
+# Output
+###########################
 
 def write_to_json(data, filename):
     with io.open(os.path.join(SAVE_PATH, filename), 'w', encoding='utf8') as f:
         f.write(unicode(json.dumps(data, ensure_ascii=False)))
 
 
-def parse_and_save((name, url)):
+def parse_and_save((name, week)):
     try:
-        data = parse_restaurant(name, url)
+        data = parse_restaurant(name, week)
     except Exception:
         logging.exception(name + " - Failed to parse")
         data = []
@@ -149,16 +220,14 @@ def parse_and_save((name, url)):
 def main():
     # Configure the logger
     logging.basicConfig(filename='menuparser.log', level='WARNING')
-
+    
+    content = load_and_split(URL)
+    
     # Parse and save the 2 restaurants
     pool = ThreadPool(4)
-    pool.map(parse_and_save, [
-        ('Etterbeek.nl', URL_ETTERBEEK_NL),
-        ('Jette.nl', URL_JETTE_NL),
-        ('Etterbeek.en', URL_ETTERBEEK_EN),
-        ('Jette.en', URL_JETTE_EN),
-    ])
+    pool.map(parse_and_save, content.items())
 
 
 if __name__ == "__main__":
     main()
+
